73fe3f6c828cbe6c9644dc9b42cc3ff48710d76a
kasheemlew <417879751@qq.com>
2017-05-03 07:26:01 +0800

    seperated from restccnu

--- service/api.py
+++ service/api.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
+import os
+import sys
 import json
-from flask import Blueprint, jsonify, request
-from paginate import _Pagination
-from spider import search_books, get_book, book_me, renew_book
-from decorator import require_lib_login, tojson
-from models import connection, Attention
+from flask import request, jsonify, Blueprint
+from .models import connection, Week
+
 
 api = Blueprint(
         'api',
@@ -13,178 +12,24 @@ api = Blueprint(
         )
 
 
-@api.route('/lib/search/')
-@tojson
-def api_search_books():
+@api.route('/classroom/get_classroom/', methods=['GET'])
+def api_get_classrooom():
     """
-    :function: api_search_books
+    :function: api_get_classroom
     :args: none
-    :rv: 图书信息
 
-    搜索图书, 返回图书相关信息, 分页(每页20条)
+    获取空闲教室表
     """
-    per_page = 20
-    keyword = request.args.get('keyword')
-    page = int(request.args.get('page') or '1')
-    if keyword:
-        book_info_list = search_books(keyword)
-        pg_book_info_list = _Pagination(book_info_list, page, per_page)
-        return {'meta': {
-                'max': pg_book_info_list.max_page,
-                'per_page': per_page },
-            'results': book_info_list[(page-1)*per_page:page*per_page]}
+    weekno = request.args.get('weekno')
+    weekday = request.args.get('weekday')
+    building = request.args.get('building')
 
-
-@api.route('/lib/')
-def api_book_detail():
-    """
-    :function: api_book_detail
-    :args: none
-    :rv: 图书详细信息
-
-    图书详情
-    """
-    id = request.args.get('id')
-    return jsonify(get_book(id))
-
-
-@api.route('/lib/me/')
-@require_lib_login
-@tojson
-def api_book_me(s, sid):
-    """
-    :function: api_book_me
-    :args:
-        - s: 爬虫session对象
-        - sid: 学号
-    """
-    return book_me(s)
-
-
-@api.route('/lib/renew/', methods=['POST'])
-@require_lib_login
-def api_renew_book(s, sid):
-    """
-    :function: api_renew_book
-    :args:
-        - s: 爬虫session对象
-        - bar_code: 图书bar_code字段
-        - check: 图书check字段
-    """
-    bar_code = request.get_json().get('bar_code')
-    check = request.get_json().get('check')
-    res_code = renew_book(s, bar_code, check)
-    return jsonify({}), res_code
-
-
-@api.route('/lib/create_atten/', methods=['POST'])
-@require_lib_login
-def api_create_atten(s, sid):
-    """
-    :function: api_create_atten
-    :args:
-        - s: 爬虫session对象
-        - sid: 学号
-
-    添加关注图书, 存储mongodb数据库
-    """
-    def init_atten(connection, book_name, book_id, book_bid, book_author, sid):
-        """提醒初始化"""
-        atten = connection.Attention()
-        atten['bid'] = book_bid
-        atten['book'] = book_name
-        atten['id'] = book_id
-        atten['author'] = book_author
-        atten['sid'] = sid
-        atten.save()
-        return atten
-
-    if request.method == 'POST':
-        book_bid = request.get_json().get('bid')
-        book_name = request.get_json().get('book')
-        book_id = request.get_json().get('id')
-        book_author = request.get_json().get('author')
-        print 'ok'
-
-        if not(book_bid and book_name and book_id and book_author):
-            return
-        print 'ok'
-
-        atten = connection.Attention.find_one({
-            'bid': book_bid,
-            'book': book_name,
-            'id': book_id,
-            'author': book_author,
-            'sid': sid
-        })
-        print 'ok'
-        if atten:
-            return jsonify({}), 409
-        print 'ok'
-
-        atten = init_atten(connection, book_name, book_id, book_bid, book_author, sid)
-        print 'ok'
-        atten.save()
-        print 'ok'
-        return jsonify({}), 201
-
-
-@api.route('/lib/get_atten/')
-@require_lib_login
-def api_get_atten(s, sid):
-    """
-    :function: api_get_atten
-    :args:
-        - s: 爬虫session对象
-        - sid: 学号
-
-    获取关注的图书列表
-    """
-    def isavailable(book_id):
-        """获取图书是否可借"""
-        book_list = get_book(book_id)
-        for book in book_list['books']:
-            if book['status'].encode('utf-8') == '\xe5\x8f\xaf\xe5\x80\x9f': return "y"
-        return "n"
-
-    all_list = list()
-    available_list = list()
-
-    attens = connection.Attention.find({'sid': sid})
-    try: attens[0]
-    except IndexError: return jsonify({}), 404
-
-    for each_atten in attens:
-        all_list.append({
-            "bid": each_atten['bid'],
-            "book": each_atten['book'],
-            "id": each_atten['id'],
-            "author": each_atten['author'],
-            "avbl": isavailable(each_atten['id'])
-        })
-
-    return jsonify(all_list), 200
-
-
-@api.route('/lib/del_atten/', methods=['DELETE'])
-@require_lib_login
-def api_del_atten(s, sid):
-    """
-    :function: api_del_atten
-    :args:
-        - s: 爬虫session对象
-        - sid: 学号
-
-    删除图书关注提醒
-    """
-    if request.method == 'DELETE':
-        book_id = request.get_json().get('id')
-
-        atten = connection.Attention.find_one({
-            'id': book_id,
-            'sid': sid,
-        })
-
-        if not atten: return jsonify({}), 404
-        atten.delete()
-        return jsonify({}), 200
+    try:
+        week = connection.Week.find_one({
+            'weekNo': unicode('week'+weekno),
+            'bno': unicode(building)
+            })
+        classroom_list = week[weekday]
+        return jsonify(classroom_list)
+    except:
+        return jsonify({}), 502
